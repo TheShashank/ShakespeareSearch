146b9fccbb47c99179941e6dc472f10c8c99825a
Shashank Rammoorthy <shashankr.blr@gmail.com>
2017-12-12 16:49:41 +0530

    Changed

--- serv.py
+++ serv.py
@@ -52,6 +52,6 @@ def search():
     '''
 
 if os.environ.get('APP_LOCATION') == 'heroku':
-    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), server='paste')
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 else:
     run(host='localhost', port=8080, reloader=True)
